e584a8f5c267e5a95c49798136cad2a738e61fcb
fmaatoug <maatougfares@gmail.com>
2023-06-04 14:46:45 +0200

    refactor generalize rule in one method

--- kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.java
+++ kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.java
@@ -6,11 +6,32 @@ import org.springframework.stereotype.Component;
 public class KataForFunService {
 
     public String convertNumber(int number) {
-        if ( number == 3 ) return "KataKata";
-        if ( number == 5 ) return "ForFor";
-        if ( number == 7 ) return "Fun";
-        if ( number == 9 ) return "Kata";
-        return String.valueOf(number);
+        return returnResultBasedOnRule(number);
+    }
+
+    private String returnResultBasedOnRule(int number ){
+        StringBuilder sb = new StringBuilder();
+        String inputString = String.valueOf(number);
+        if (number % 3 == 0) {
+            sb.append("Kata");
+        }
+        if (number % 5 == 0) {
+            sb.append("For");
+        }
+        if (inputString.contains("3")) {
+            sb.append("Kata");
+        }
+        if (inputString.contains("5")) {
+            sb.append("For");
+        }
+        if (inputString.contains("7")) {
+            sb.append("Fun");
+        }
+        if (sb.length() == 0) {
+            return inputString;
+        } else {
+            return sb.toString();
+        }
     }
 
 }

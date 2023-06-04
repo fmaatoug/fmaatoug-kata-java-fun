70c728a4075e583a8288559fc23f074c6cb52d32
fmaatoug <maatougfares@gmail.com>
2023-06-04 16:02:06 +0200

    Add working code + refactor green for 53 return ForKata

--- kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.java
+++ kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.java
@@ -2,6 +2,9 @@ package com.kata.kataforfun.services;
 
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class KataForFunService {
 
@@ -18,20 +21,25 @@ public class KataForFunService {
         if (number % 5 == 0) {
             sb.append("For");
         }
-        if (inputString.contains("3")) {
-            sb.append("Kata");
-        }
-        if (inputString.contains("5")) {
-            sb.append("For");
-        }
-        if (inputString.contains("7")) {
-            sb.append("Fun");
+        if (inputString.contains("3") || inputString.contains("5") || inputString.contains("7")) {
+               inputString.chars().mapToObj(c -> (char) c).
+                map(KataForFunService::mapValueToContainedCharacter).forEach(sb::append);
         }
         if (sb.length() == 0) {
             return inputString;
         } else {
             return sb.toString();
         }
+    }
+
+    private static String mapValueToContainedCharacter(Character c){
+        Map<Character, String> correspondingMatch = new HashMap<>();
+        correspondingMatch.put('3', "Kata");
+        correspondingMatch.put('5', "For");
+        correspondingMatch.put('7', "Fun");
+
+        return correspondingMatch.get(c);
+        
     }
 
 }

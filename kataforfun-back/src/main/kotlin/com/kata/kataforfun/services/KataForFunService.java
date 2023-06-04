8f6e71e7b5334cae5d52d7b6eeef6bc24bc36ddf
fmaatoug <maatougfares@gmail.com>
2023-06-04 13:16:15 +0200

    Add minimal working code for 3 returning KataKata green

--- kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.java
+++ kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 public class KataForFunService {
 
     public String convertNumber(int number) {
+        if ( number == 3 ) return "KataKata";
         return String.valueOf(number);
     }
 

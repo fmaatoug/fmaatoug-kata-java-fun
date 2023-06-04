c4792e89d613c91dc8fae9170a1a9012f528bcbd
fmaatoug <maatougfares@gmail.com>
2023-06-04 13:41:42 +0200

    Add minimal working code for 7 returning Fun green

--- kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.java
+++ kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.java
@@ -8,6 +8,7 @@ public class KataForFunService {
     public String convertNumber(int number) {
         if ( number == 3 ) return "KataKata";
         if ( number == 5 ) return "ForFor";
+        if ( number == 7 ) return "Fun";
         return String.valueOf(number);
     }
 

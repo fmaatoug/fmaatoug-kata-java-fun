888c130fc3eaf808edd3aec79044a6b1fa1b2c49
fmaatoug <maatougfares@gmail.com>
2023-06-04 12:54:41 +0200

    Add minimal failing test for 2 returning 2 red

--- kataforfun-back/src/test/kotlin/com/kata/kataforfun/KataForFunApplicationTests.java
+++ kataforfun-back/src/test/kotlin/com/kata/kataforfun/KataForFunApplicationTests.java
@@ -13,6 +13,11 @@ class KataForFunApplicationTests {
         Assertions.assertThat(new KataForFunService().convertNumber(1)).isEqualTo("1");
     }
 
+    @Test
+    void Should_return_2_if_number_is_2(){
+        Assertions.assertThat(new KataForFunService().convertNumber(2)).isEqualTo("2");
+    }
+
 }
 
 

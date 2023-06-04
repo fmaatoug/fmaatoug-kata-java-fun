ac4b2b943af2900590bec5ffb27d954ff7dbb0a8
fmaatoug <maatougfares@gmail.com>
2023-06-04 13:46:25 +0200

    Add minimal working code for 9 returning Kata green

--- kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.java
+++ kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.java
@@ -9,6 +9,7 @@ public class KataForFunService {
         if ( number == 3 ) return "KataKata";
         if ( number == 5 ) return "ForFor";
         if ( number == 7 ) return "Fun";
+        if ( number == 9 ) return "Kata";
         return String.valueOf(number);
     }
 
